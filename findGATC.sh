11ec05cf37bedc4714400368db0d5e1a7c9558e5
Eti Sinha <sinhaeti@yahoo.com>
2016-07-13 19:40:29 -0700

    Add findGATC file

---
+++ findGATC.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Name: findGATC
+# Purpose: In a input genome sequence, find the cutter sites. The cutter sites are where an enzyme will cut on a DNA strand, at the sequende GATC and CTAG
